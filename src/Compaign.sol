966974dc39495da0f2bd8b21bd022d22790f09f2
domambia <omambiadauglous@gmail.com>
2024-06-02 15:50:43 +0300

    Fix the compaign project

--- src/Compaign.sol
+++ src/Compaign.sol
@@ -2,11 +2,112 @@
 pragma solidity ^0.8.13;
 
 contract Compaign {
+    struct Approver {
+        address addr;
+        string name;
+        string email;
+        uint256 amount;
+    }
+
+    struct Request {
+        string description;
+        uint256 value;
+        address recipient;
+        bool complete;
+        uint256 approvalCount;
+        mapping(address => bool) approvals;
+    }
     address public manager;
     uint256 public minimumContribution;
+    mapping(address => Approver) public approvers;
+    Request[] public requests;
+    uint approversCount;
 
-    constructor(uint _minimum) public {
+    modifier onlyManager() {
+        require(msg.sender == manager, "Only manager can call");
+        _;
+    }
+
+    constructor(uint _minimum) {
         manager = msg.sender;
         minimumContribution = _minimum;
+    }
+
+    function contribute(
+        string memory _name,
+        string memory _email
+    ) public payable {
+        //  check if the minimum contribution is met
+        require(
+            msg.value > minimumContribution,
+            "Minimum contribution not met"
+        );
+        Approver memory _approver = Approver({
+            addr: msg.sender,
+            email: _email,
+            name: _name,
+            amount: msg.value
+        });
+        approvers[msg.sender] = _approver;
+        approversCount++;
+    }
+
+    function createRequest(
+        string memory _description,
+        uint256 _value,
+        address _recipient
+    ) public onlyManager {
+        Request storage newRequest = requests.push();
+        newRequest.description = _description;
+        newRequest.value = _value;
+        newRequest.recipient = _recipient;
+        newRequest.complete = false;
+        newRequest.approvalCount = 0;
+    }
+
+    function approveRequest(uint256 index) public {
+        Request storage request = requests[index];
+        require(approvers[msg.sender].amount > 0, "You are not a contributor");
+        require(
+            !request.approvals[msg.sender],
+            "You have already approved this request"
+        );
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function getRequestDetails(
+        uint256 index
+    )
+        public
+        view
+        returns (
+            string memory description,
+            uint256 value,
+            address recipient,
+            bool complete,
+            uint256 approvalCount
+        )
+    {
+        Request storage request = requests[index];
+        return (
+            request.description,
+            request.value,
+            request.recipient,
+            request.complete,
+            request.approvalCount
+        );
+    }
+
+    function finalizeRequest(uint index) public onlyManager {
+        Request storage request = requests[index];
+        require(!request.complete, "Request already completed");
+        require(
+            request.approvalCount > (approversCount / 2),
+            "Approval count not met"
+        );
+        payable(request.recipient).transfer(request.value);
+        request.complete = true;
     }
 }
--- test/Compaign.t.sol
+++ test/Compaign.t.sol
@@ -4,19 +4,77 @@ pragma solidity ^0.8.13;
 import {Test, console} from "forge-std/Test.sol";
 import {Compaign} from "../src/Compaign.sol";
 
-contract CounterTest is Test {
+contract CompaignTest is Test {
     Compaign public compaign;
+    string public contributorName = "Omambia M";
+    string public contributorEmail = "support@omambia.dev";
+    address public manager = address(0x10000);
+    address public contributor1Address = address(0x20001);
+    address public contributorA2ddress = address(0x20002);
+    address public vendorAddress = address(0x30000);
     uint256 public minimumContribution = 0.2 ether;
+    uint256 public contributedAmount = 0.3 ether;
 
     function setUp() public {
+        vm.deal(manager, 10 ether);
+        vm.startPrank(manager);
         compaign = new Compaign(minimumContribution);
+        vm.stopPrank();
     }
 
-    function test_contract_deployed() public view {
-        assertEq(compaign.manager(), address(this));
+    function test_contract_deployed() public {
+        console.log("Manager Address: ", manager);
+        vm.startPrank(manager);
+        assertEq(compaign.manager(), manager);
         console.log("Compaign Address: ", address(compaign));
         console.log("Minimum Contribution: ", compaign.minimumContribution());
         console.log("Compaign Manager: ", compaign.manager());
         assertEq(compaign.minimumContribution(), minimumContribution);
+        vm.stopPrank();
+    }
+
+    function test_contribute() public payable {
+        vm.deal(contributor1Address, 1 ether);
+        vm.startPrank(contributor1Address);
+        compaign.contribute{value: contributedAmount}(
+            contributorName,
+            contributorEmail
+        );
+        (
+            address addr,
+            string memory name,
+            string memory email,
+            uint256 amount
+        ) = compaign.approvers(contributor1Address);
+
+        console.log("Contributor Address: ", addr);
+        console.log("Contributor Name: ", name);
+        console.log("Contributor Email: ", email);
+        console.log("Contributor Amount: ", amount);
+        assertEq(addr, contributor1Address);
+        assertEq(name, contributorName);
+        assertEq(email, contributorEmail);
+        assertEq(amount, contributedAmount);
+        vm.stopPrank();
+    }
+
+    function test_createRequest() public {
+        vm.deal(manager, 10 ether);
+        vm.startPrank(manager);
+        compaign.createRequest("Buy a new laptop", 5 ether, vendorAddress);
+        (
+            string memory description,
+            uint256 value,
+            address recipient,
+            bool complete,
+            uint256 approvalCount
+        ) = compaign.getRequestDetails(0);
+        assertEq(value, 5 ether);
+        assertEq(approvalCount, 0);
+        assertEq(complete, false);
+        assertEq(recipient, vendorAddress);
+        console.log("Request Description: ", description);
+        console.log("Request Value: ", value);
+        vm.stopPrank();
     }
 }
